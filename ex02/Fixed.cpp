5e464db4d6d2c7bf825db91b13ae68d542f210d5
Duchemil <Lucas-76700@hotmail.fr>
2025-01-09 12:14:18 +0100

    cpp02 finished

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -116,3 +116,19 @@ Fixed Fixed::operator--(int) {
 	this->_fixedPointValue--;
 	return tmp;
 }
+
+Fixed &Fixed::min(Fixed &a, Fixed &b) {
+	return (a < b) ? a : b;
+}
+
+const Fixed &Fixed::min(const Fixed &a, const Fixed &b) {
+	return (a < b) ? a : b;
+}
+
+Fixed &Fixed::max(Fixed &a, Fixed &b) {
+	return (a > b) ? a : b;
+}
+
+const Fixed &Fixed::max(const Fixed &a, const Fixed &b) {
+	return (a > b) ? a : b;
+}
--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -26,13 +26,16 @@ class Fixed {
 		Fixed operator-(const Fixed &other) const;
 		Fixed operator*(const Fixed &other) const;
 		Fixed operator/(const Fixed &other) const;
-		
+
 		Fixed &operator++();
 		Fixed operator++(int);
 		Fixed &operator--();
 		Fixed operator--(int);
 
-
+		static Fixed &min(Fixed &a, Fixed &b);
+		static const Fixed &min(const Fixed &a, const Fixed &b);
+		static Fixed &max(Fixed &a, Fixed &b);
+		static const Fixed &max(const Fixed &a, const Fixed &b);
 		~Fixed();
 
 		int getRawBits(void) const;
